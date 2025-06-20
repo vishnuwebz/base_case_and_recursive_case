424cbc1a1ba876850a76eb40c41f5a3be0e3fbde
Vishnu G <vishnugkannath@gmail.com>
2025-06-20 16:54:09 +0530

    5th commt - added merger_sort project

---
+++ 171_merge_sort.py
@@ -0,0 +1,29 @@
+# 5 merge sort
+
+def merge_sort(lst):
+    if len(lst) <= 1:
+        return lst
+    mid = len(lst) // 2
+    left = merge_sort(lst[:mid])
+    right = merge_sort(lst[mid:])
+    return merge(left, right)
+
+def merge(left, right):
+    result = []
+    i = j = 0
+    while i < len(left) and j < len(right):
+        if left[i] <= right[i]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    result.extend(left[i:])
+    result.extend(right[j:])
+    return result
+
+print(merge_sort([3, 1, 4, 2]))  # [1, 3, 2, 4]
+
+"""
+Divides list into smaller sorted lists, merges them in order. Merge compares elements systematically.
+"""
