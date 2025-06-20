4fe36dab7b0edf33a432ef1d77f7f14c51165dce
Vishnu G <vishnugkannath@gmail.com>
2025-06-20 16:01:33 +0530

    added 2nd mini project based on flatten list

---
+++ 168_Flatten_List.py
@@ -0,0 +1,15 @@
+# 2 Flatten List
+
+def flatten(lst):
+    result = []
+    for item in lst:
+        if isinstance(item, list):
+            result.extend(flatten(item))
+        else:
+            result.append(item)
+    return result
+
+print(flatten([1, [2, [3, 4], 5]]))
+
+# [1, 2, 3, 4, 5]
+
